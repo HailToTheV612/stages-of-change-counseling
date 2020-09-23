33eed8eb484f3e48a3462281cead46d478ea28de
Vanessa Morales <vanessamorales@Vanessas-MacBook-Pro.local>
2020-09-23 17:11:03 -0400

    Added smooth scrolling to script.js. Fixed padding of main-text and header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+/*Smooth Scrolling */
+
+$("#navbarNav .navbar-nav li .nav-link").on('click', function(smooth) {
+
+    //set hash
+    var target = this.hash;
+    
+    //prevent default clicking behavior
+    smooth.preventDefault();
+    
+    //grab height of the navbar
+    var navOffset = $('#navbar').height();
+    
+    //animate the scroll
+    return $('html, body').animate({
+      scrollTop: $(this.hash).offset().top - navOffset
+    }, 1000, function() {
+        //add hash to the end of URL
+      return window.history.pushState(null, null, target);
+    });
+  });
+
+/*Auto close navbar toggler when clicked on*/
 $('.navbar-collapse .nav-link').click(function(){
     $(".navbar-collapse").collapse('hide');
 });
