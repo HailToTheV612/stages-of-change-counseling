bb130b9c7464dd5d492cb39bce3ca4e24b9aec9b
Vanessa Morales <vanessamorales@Vanessas-MacBook-Pro.local>
2020-09-27 12:54:37 -0400

    Changed the speed of smooth scrolling. Added a media query for margin 0 of services list items. Changed col breakpoints of services item lists

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ $("#navbarNav .navbar-nav li .nav-link").on('click', function(smooth) {
     //prevent default clicking behavior
     smooth.preventDefault();
     
-    //grab height of the navbar
+    //grab height of navbar
     var navOffset = $('#navbar').height();
     
     //animate the scroll
     return $('html, body').animate({
       scrollTop: $(this.hash).offset().top - navOffset
-    }, 1000, function() {
+    }, 1500, function() {
         //add hash to the end of URL
       return window.history.pushState(null, null, target);
     });
